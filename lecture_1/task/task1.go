0ba1b0cfc99d483712d63cc6b6f955baabd60b7d
JuricaJuric <jurica.juric@ag04.com>
2021-05-08 14:50:18 +0200

    Implement first task

---
+++ lecture_1/task/task1.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"github.com/sethgrid/pester"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type application struct {
+	Name string
+	Age int
+	Passed bool
+	Skills []string
+}
+
+const URL = "https://run.mocky.io/v3/f7ceece5-47ee-4955-b974-438982267dc8"
+
+func main() {
+	httpClient := pester.New()
+	//httpClient.Backoff = linearBackoff
+
+	httpResponse, err := httpClient.Get(URL)
+	if err != nil {
+		log.Fatal(
+			errors.WithMessage(err, "HTTP get towards API"),
+		)
+	}
+
+	bodyContent, err := ioutil.ReadAll(httpResponse.Body)
+	if err != nil {
+		log.Fatal(
+			errors.WithMessage(err, "reading body of API response"),
+		)
+	}
+
+	var decodedContent []application
+	err = json.Unmarshal(bodyContent, &decodedContent)
+	if err != nil {
+		log.Fatal(
+			errors.WithMessage(err, "unmarshalling the JSON body content"),
+		)
+	}
+
+	f, err := os.Create("people.txt")
+	if err != nil {
+		log.Fatal(
+			errors.WithMessage(err, "opening a file"),
+		)
+	}
+	defer f.Close()
+
+	for _, val := range decodedContent {
+		if val.Passed {
+			var HasJava = false
+			var HasGo = false
+			for _, skill := range val.Skills {
+				if skill == "Java" { HasJava = true }
+				if skill == "Go" { HasGo = true }
+			}
+			if HasJava || HasGo {
+				var data string
+				data += val.Name
+				data += " - "
+				for idx, skill := range val.Skills {
+					data += skill
+					if idx != len(val.Skills) - 1 {
+						data += ", "
+					} else {
+						data += "\n"
+					}
+				}
+				f.WriteString(data)
+			}
+		}
+	}
+
+}
+
+
+
