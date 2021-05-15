4ac2cae891fdb3ae3ec05b9e63f760fcef7ae0f9
Jurica Jurić <juricajuric@Juricas-MacBook-Pro.local>
2021-05-15 20:53:04 +0200

    Finish last task from second lecture

--- lecture_2/05_offerfeed/cmd/main.go
+++ lecture_2/05_offerfeed/cmd/main.go
@@ -1,45 +1,16 @@
 package main
 
 import (
-	"code-cadets-2021/lecture_2/05_offerfeed/internal/domain/services"
-	"code-cadets-2021/lecture_2/05_offerfeed/internal/infrastructure/http"
-	"code-cadets-2021/lecture_2/05_offerfeed/internal/infrastructure/queue"
-	"context"
-	stdhttp "net/http"
-	"time"
+	"code-cadets-2021/lecture_2/05_offerfeed/cmd/bootstrap"
+	"code-cadets-2021/lecture_2/05_offerfeed/internal/tasks"
 )
 
 func main() {
+	signalHandler := bootstrap.NewSignalHandler()
 
-	newQueue := queue.NewOrderedQueue()
-	/*source := newQueue.GetSource()
+	feed := bootstrap.NewAxilisOfferFeed()
+	queue := bootstrap.NewOrderedQueue()
+	feedProcessorService := bootstrap.NewFeedProcessorService(feed, queue)
 
-	source <- models.Odd{
-		Id:          "",
-		Name:        "",
-		Match:       "",
-		Coefficient: 0,
-		Timestamp:   time.Time{},
-	}
-
-	close(source)
-
-	newQueue.Start(nil)*/
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	feed := http.NewAxilisOfferFeed(stdhttp.Client{})
-
-	go feed.Start(ctx)
-	go newQueue.Start(ctx)
-
-	service := services.NewFeedProcessorService(feed, newQueue)
-
-	go service.Start(ctx)
-
-	time.Sleep(time.Second * 3)
-
-	cancel()
-
-	time.Sleep(time.Second * 3)
+	tasks.RunTasks(signalHandler, feed, queue, feedProcessorService)
 }
