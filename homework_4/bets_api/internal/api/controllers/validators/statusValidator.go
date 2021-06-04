36778c052c4bf7a9865efec0589d7a8564262e4a
Jurica Jurić <juricajuric@Juricas-MacBook-Pro.local>
2021-06-04 21:09:00 +0200

    Fix per code review

--- homework_4/bets_api/internal/api/controllers/validators/statusValidator.go
+++ homework_4/bets_api/internal/api/controllers/validators/statusValidator.go
@@ -2,10 +2,14 @@ package validators
 
 type StatusValidator struct {}
 
+const statusActive = "active"
+const statusWon = "won"
+const statusLost = "lost"
+
 func NewStatusValidator() *StatusValidator {
 	return &StatusValidator{}
 }
 
 func (s *StatusValidator) StatusIsValid(status string) bool {
-	return status == "active" || status == "lost" || status == "won"
+	return status == statusActive || status == statusLost || status == statusWon
 }
